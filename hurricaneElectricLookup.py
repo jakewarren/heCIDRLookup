a068afb80bdfc515f9dc3976b48d1549b3ef9e55
Jake Warren <jakewarren@gmail.com>
2015-05-12 17:28:36 -0500

    added sorting based on company name

--- hurricaneElectricLookup.py
+++ hurricaneElectricLookup.py
@@ -2,48 +2,64 @@
 
 """
 -------------------------------------------------------------------------------
-Name:		hurricaneElectricLookup.py
-Purpose:	Look up target network ranges using bgp.he.net
-Author:		Justin Kennedy (@jstnkndy)
+Name:        hurricaneElectricLookup.py
+Purpose:    Look up target network ranges using bgp.he.net
+Author:        Justin Kennedy (@jstnkndy)
 -------------------------------------------------------------------------------
 """
 
 from bs4 import BeautifulSoup
 import requests
 import sys
+import re
 
 def usage():
-	print "Usage: %s <search string>" % sys.argv[0]
+    print "Usage: %s <search string>" % sys.argv[0]
 
 
 def main():
-	if len(sys.argv) != 2:
-		usage()
-		sys.exit()
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit()
 
-	search = sys.argv[1]
+    search = sys.argv[1]
 
-	headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36"}
-	url = "http://bgp.he.net/search?search%5Bsearch%5D=" + search + "&commit=Search"
-	browser = requests.get(url, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36"}
+    url = "http://bgp.he.net/search?search%5Bsearch%5D=" + search + "&commit=Search"
+    browser = requests.get(url, headers=headers)
 
-	soup = BeautifulSoup(browser.text)
-	table = soup.find("table")
-	
-	try:
-		rows = table.findAll("tr")
-	except:
-		print "No results found"
-		sys.exit()
+    soup = BeautifulSoup(browser.text)
+    table = soup.find("table")
+    
+    try:
+        rows = table.findAll("tr")
+    except:
+        print "No results found"
+        sys.exit()
 
-	for row in rows:
-		tds = row.findAll("td")
-		try:
-			a = str(tds[0].get_text())
-			b = str(tds[1].get_text())
-			print "%s, %s" % (a,b)
-		except:
-			continue
+    #dictionary to store the cidrs
+    cidrDict = dict()
+
+    for row in rows:
+        tds = row.findAll("td")
+        try:
+            cidr = str(tds[0].get_text())
+            company = str(tds[1].get_text())
+
+            #extract IPv4 CIDRs only
+            if not re.match(ur'\d+\.\d+\.\d+\.\d+/\d{1,2}',cidr):
+                continue
+            
+        except:
+            continue
+        
+        cidrDict.setdefault(company,[]).append(cidr)
+
+    for key in cidrDict:
+        print key+":"
+        print "\n".join(cidrDict[key])
+        print ""
+        
 
 if __name__ == "__main__":
-	main()
+    main()
